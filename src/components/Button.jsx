feaf2b715720dad241155f0fd60bdde60b3cda7f
eunseo <eundeok9@gmail.com>
2023-11-01 22:04:34 +0900

    feat: update Button Component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,19 +3,82 @@ import styled from "styled-components";
 const Button = styled.button`
   /* default는 작은 파란 버튼 */
   border: none;
-  border-radius: 15px;
-  width: 180px;
-  height: 60px;
-  font-size: 20px;
+  border-radius: 20px;
   color: white;
-  background-color: #0032b7;
   cursor: pointer;
 
-  /* large가 넘어오면 큰 파란색 버튼 */
-  ${(props) => props.large && `width: 200px; height: 50px;`}
+  /* login이나 join이 넘어오면 로그인, 회원가입 버튼 */
+  ${(props) =>
+    (props.login || props.join) &&
+    `width: 160px;
+   height: 40px;
+   background-color: #006ED3;
+   font-size: 16px;
+   `}
 
-  /* red가 넘어오면 작은 빨간 버튼 */
-  ${(props) => props.red && `background-color: #FF0000;`}
+  ${(props) =>
+    props.stockPageBuy &&
+    `
+    width: 80px;
+    height: 40px;
+    font-size: 18px;
+    background-color: #006ED3;
+  `}
+
+${(props) =>
+    props.stockPageSell &&
+    `
+    width: 80px;
+    height: 40px;
+    font-size: 18px;
+    background-color: #FF7456;
+  `}
+
+${(props) =>
+    props.stockModalBuy &&
+    `
+    width: 180px;
+    height: 60px;
+    font-size: 24px;
+    background-color: #006ED3;
+  `}
+
+${(props) =>
+    props.stockModalSell &&
+    `
+    width: 180px;
+    height: 60px;
+    font-size: 24px;
+    background-color: #FF7456;
+  `}
+
+${(props) =>
+    props.projectPageNew &&
+    `
+    width: 120px;
+    height: 40px;
+    font-size: 16px;
+    background-color: #006ED3;
+  `}
+
+  
+${(props) =>
+    props.projectModalNew &&
+    `
+    width: 150px;
+    height: 50px;
+    font-size: 20px;
+    background-color: #006ED3;
+  `}
+
+${(props) =>
+    props.projectModalCancel &&
+    `
+    width: 150px;
+    height: 50px;
+    font-size: 20px;
+    background-color: #FF7456;
+  `}
 `;
 
 export default Button;
