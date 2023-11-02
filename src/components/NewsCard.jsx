f03c19b5bfbb03b03f72f045d06e174c496e36f6
or_hyn <dhflxhdxhd@gmail.com>
2023-11-02 22:29:04 +0900

    Style : 뉴스 카드 스타일 수정 #6
    뉴스카드 타이틀 위치 고정.

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -9,6 +9,8 @@ const CardContainer = styled.div`
   box-shadow: 0px 2px 8px 0px rgba(0, 0, 0, 0.15);
   display: flex;
   flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 const NewsTitle = styled.p`
@@ -17,8 +19,15 @@ const NewsTitle = styled.p`
   font-family: Montserrat;
   font-size: 1.0625rem;
   font-style: normal;
-  font-weight: 600;
+  font-weight: 700;
   line-height: normal;
+  margin-bottom: 10px;
+  width: 80%;
+  height: 50%;
+
+  overflow-wrap: break-word; /* 단어가 너무 길면 줄바꿈 */
+  word-wrap: break-word; /* 이전 브라우저 호환성을 위해 */
+  overflow: hidden; /* 넘치는 텍스트 숨기기 */
 `;
 
 const NewsDate = styled.p`
@@ -27,8 +36,10 @@ const NewsDate = styled.p`
   font-family: Montserrat;
   font-size: 0.75rem;
   font-style: normal;
-  font-weight: 400;
+  font-weight: 300;
   line-height: normal;
+  margin-top: 30px;
+  padding-bottom: 20px;
 `;
 
 const NewIcon = styled.p`
@@ -37,8 +48,10 @@ const NewIcon = styled.p`
   font-family: Pretendard Variable;
   font-size: 0.625rem;
   font-style: normal;
-  font-weight: 500;
+  font-weight: 600;
   line-height: normal;
+  margin-bottom: 10px;
+  margin-top: 30px;
 `;
 
 const NewsCard = ({ title, date }) => {
